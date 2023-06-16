45cb177ad21e15631d726804a2cde4ab0d034437
Carlos Damiao <carlosdamiao9999@gmail.com>
2023-06-16 11:47:59 -0300

    commit add db

--- IA/settings.py
+++ IA/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600,conn_health_checks=True,)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
