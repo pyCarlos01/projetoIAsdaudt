e3384657c11dcf39787c414072748d9cd8298775
Carlos Damiao <carlosdamiao9999@gmail.com>
2023-06-17 13:41:48 -0300

    commit imagens

--- IAs/views.py
+++ IAs/views.py
@@ -1,8 +1,9 @@
 import os
+import xlwt
 import pandas as pd
 from .forms import *
 from .models import *
-from datetime import date
+from datetime import date, datetime
 from django.views.generic import *
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
@@ -461,6 +462,39 @@ def horas_extras(request, data, arq):
     except:
         return redirect('IAs:homefrotas')
     return render(request, 'colaboradores.html')
+
+def export(model, filename, queryset, columns):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet(model)
+
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    default_style = xlwt.XFStyle()
+    rows = queryset
+
+    for row, rowdata in enumerate(rows):
+        row_num +=1
+        for col, val in enumerate (rowdata):
+            ws.write(row_num, col, val, default_style)
+    wb.save(response)
+
+def exportar_tab(request):
+    model = Frota
+    filename = 'nome arq'
+    queryset = Frota.objects.all().values_list('numero', 'placa')
+    columns = ('Frota', 'Placa')
+    response = export(model, filename, queryset, columns)
+    return response
+
 
 
 
