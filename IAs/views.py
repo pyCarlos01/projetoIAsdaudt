c6bb9a64cb0883714f6d227e60a8ad9a2ebcf409
Carlos Damiao <carlosdamiao9999@gmail.com>
2023-06-17 15:14:54 -0300

    commit downloads

--- IAs/views.py
+++ IAs/views.py
@@ -335,8 +335,8 @@ def download_horario_saida(request, data):
 def download_colaboradores(request, funcao, status, empresa):
     # try:
         if request.method == 'POST':
-            if funcao == 'TODOS' and status == 'TODOS' and empresa == 'TODOS':
-                queryset = Colaboradores.objects.all()
+            # if funcao == 'TODOS' and status == 'TODOS' and empresa == 'TODOS':
+                # queryset = Colaboradores.objects.all()
             # elif funcao == 'TODOS' and status != 'TODOS' and empresa != 'TODOS':
             #     queryset = Colaboradores.objects.filter(status=status, empresa=empresa).all()
             # elif funcao == 'TODOS' and status == 'TODOS' and empresa != 'TODOS':
@@ -351,22 +351,22 @@ def download_colaboradores(request, funcao, status, empresa):
             #     queryset = Colaboradores.objects.filter(status=status).all()
 
             # Crie um DataFrame com os dados do modelo
-            df = pd.DataFrame(list(queryset.values()))
-
-            # Defina o caminho e nome do arquivo Excel de saída
-            output_filename =f'/Colaboradores.xlsx'
-
-            # Exporte o DataFrame para um arquivo Excel
-            df.to_excel(output_filename, index=False)
-
-            # Abra o arquivo Excel para download
-            with open(output_filename, 'rb') as file:
-                response = HttpResponse(file.read(), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-                response['Content-Disposition'] = 'attachment; filename="{}"'.format(output_filename)
-            return response
+            # df = pd.DataFrame(list(queryset.values()))
+            #
+            # # Defina o caminho e nome do arquivo Excel de saída
+            # output_filename =f'/Colaboradores.xlsx'
+            #
+            # # Exporte o DataFrame para um arquivo Excel
+            # df.to_excel(output_filename, index=False)
+            #
+            # # Abra o arquivo Excel para download
+            # with open(output_filename, 'rb') as file:
+            #     response = HttpResponse(file.read(), 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            #     response['Content-Disposition'] = 'attachment; filename="{}"'.format(output_filename)
+            # return response
     # except:
-    #     return redirect('IAs:escala')
-        return render(request, 'relatorios.html')
+            return redirect('IAs:escala')
+    #     return render(request, 'relatorios.html')
 
 def controle_func(request):
     folgas = Colaboradores.objects.filter(status='FOLGA', empresa='IPIRANGA').count()
