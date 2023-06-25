494122ef9fc7447a82567595546410141c43c937
Carlos Damiao <carlosdamiao9999@gmail.com>
2023-06-25 16:46:08 -0300

    o

--- IAs/urls.py
+++ IAs/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', Homepage.as_view(), name = 'homepage'),
     path('criarconta/', Criarconta.as_view(), name = 'criarconta'),
     path('homefrotas/', Homefrotas.as_view(), name = 'homefrotas'),
-    path('homefrotas/escala/remessa/', Remessas.as_view(), name='remessa'),
+    # path('homefrotas/escala/remessa/', Remessas.as_view(), name='remessa'),
     path('homefrotas/disponibilidade/<int:pk>', Detalhesfrotas.as_view(), name = 'detalhesfrotas'),
     path('login/', auth_view.LoginView.as_view(template_name = 'login.html'), name = 'login'),
     path('logout', auth_view.LogoutView.as_view(template_name = 'logout.html'), name = 'logout'),
@@ -17,5 +17,6 @@ urlpatterns = [
     path('homefrotas/escala/download/', download_escala, name='downloadescala'),
     path('homefrotas/relatorios/download/', Relatorios.as_view(), name='relatorios'),
     path('homefrotas/colaboradores/', controle_func, name='colaboradores'),
+    path('homefrotas/escala/remessa/', export, name='remessa'),
 
 ]
