8150fd598ce84151d9ebcbec79cbc5def433162a
Carlos Damiao <carlosdamiao9999@gmail.com>
2023-06-27 08:48:59 -0300

    atualizar bd

--- IAs/admin.py
+++ IAs/admin.py
@@ -4,8 +4,8 @@ from django.contrib.auth.admin import UserAdmin
 from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Usuario, UserAdmin)
-admin.site.register(Frota)
-admin.site.register(Colaboradores)
+# admin.site.register(Frota)
+# admin.site.register(Colaboradores)
 admin.site.register(Escala)
 # admin.site.register(Remessa)
 # Register your models here.
@@ -13,3 +13,11 @@ admin.site.register(Escala)
 @admin.register(Remessa)
 class RemessaAdmin(ImportExportModelAdmin):
     list_display = ('remessa', 'placa', 'peso', 'categoria', 'entregas', 'distancia', 'status')
+
+@admin.register(Frota)
+class RemessaAdmin(ImportExportModelAdmin):
+    list_display = ('numero', 'imagem', 'placa', 'tipo', 'bau', 'capacidade', 'motorista', 'status', 'empresa')
+
+@admin.register(Colaboradores)
+class RemessaAdmin(ImportExportModelAdmin):
+    list_display = ('nome', 'nome_guerra', 'funcao', 'status', 'periodo', 'horas', 'tempo_s', 'status1', 'empresa', 'observacoes')
